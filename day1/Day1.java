4f8f718a4962f6e57469220bf411ead826739556
Hareeskumar07 <129640005+Hareeskumar07@users.noreply.github.com>
2023-04-02 09:00:58 +0530

    Add files via upload
    Day 1 Assignments

---
+++ day1/Day1.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Day1 {
+	public static void main(String[] args) {
+		//System.out.println("Hello Selenium");
+		 	int a=20;
+		 	boolean b=true;
+		 	String C="Selenium";
+		 	System.out.println("a:"+a);
+		 	System.out.println("b:"+b);
+		 	System.out.println("C:"+C);
+	}
+
+}
---
+++ day1/assignment1.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class assignment1 {
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int c=0;
+		System.out.println(a);
+		for (int i=1;i<=11;i=i+1)
+		{
+			
+			 	c=a+b;
+			 	a=b;
+			 	b=c;
+			 	System.out.println(c);
+			 	
+		}
+	}
+
+}
